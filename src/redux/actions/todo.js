7d534d4d9f7ac547febf557f0aaa9f0a873fb04d
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-05-29 06:42:57 +0530

    Creation Actions

---
+++ src/redux/actions/todo.js
@@ -0,0 +1,19 @@
+import { ADD_TODO, DELETE_TODO } from "./action-type";
+
+//Add todo action
+export const addTodo = (todo) => ({
+  type: ADD_TODO,
+  payload: todo,
+});
+
+//Delete todo
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  payload: id,
+});
+
+//Update todo
+export const UPDATE_TODO = (id) => ({
+  type: UPDATE_TODO,
+  payload: { todo: todo, todoId: id },
+});
